7b839dd706e83493a36338a78d0afd44c90e490e
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-12-01 09:45:59 +0900

    データベース接続の初期化を追加

---
+++ pkg/database/postgres.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	// .envファイルの読み込み
+	if err := godotenv.Load(".env.local"); err != nil {
+		return nil, fmt.Errorf(".env ファイルの読み込みに失敗しました: %v", err)
+	}
+
+	// 環境変数から接続情報を取得
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
