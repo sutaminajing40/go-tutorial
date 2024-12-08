a28daa786e559eeb1d2647a987f4692263585d65
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-12-08 18:09:19 +0900

    .envファイルの読み取り方法を変更

--- backend/pkg/database/postgres.go
+++ backend/pkg/database/postgres.go
@@ -4,17 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() (*gorm.DB, error) {
-	// .envファイルの読み込み
-	if err := godotenv.Load(".env.local"); err != nil {
-		return nil, fmt.Errorf(".env ファイルの読み込みに失敗しました: %v", err)
-	}
-
 	// 環境変数から接続情報を取得
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
